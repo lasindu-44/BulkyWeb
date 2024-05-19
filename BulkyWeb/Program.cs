a486c188f0a08576b6ca13ad3ad34c8b07861d88
lasindu <lasindudulanjana44@gmail.com>
2024-05-19 21:26:34 +0530

    Category Controller

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,8 +1,11 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
